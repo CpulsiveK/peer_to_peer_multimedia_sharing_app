0545a3d3580e23224da6295062e73b63e65b893a
CpulsiveK <impulsivecliff7@gmail.com>
2023-05-13 01:00:22 +0000

    added a function to dynamically get indexer address

--- lib/network_logic/peers_utils.dart
+++ lib/network_logic/peers_utils.dart
@@ -1,7 +1,23 @@
 import 'dart:io';
 
-const String serverAddr = "192.168.46.103";
-const int port = 5050;
+Future<String?> getIndexerAddress() async {
+  const int multicastPort = 10000;
+  late String? serverAddr;
+
+  var socket =
+      await RawDatagramSocket.bind(InternetAddress.anyIPv4, multicastPort);
+  socket.joinMulticast(InternetAddress('224.0.0.1'));
+
+  await socket.forEach((event) {
+    if (event == RawSocketEvent.read) {
+      final datagram = socket.receive();
+      serverAddr = String.fromCharCodes(datagram!.data);
+      socket.close();
+    }
+  });
+
+  return serverAddr;
+}
 
 void sendRequestType({
   required Socket socket,
