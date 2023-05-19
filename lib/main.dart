bf13e110d3f79f801742cc925ea2d68ad9b69deb
CpulsiveK <impulsivecliff7@gmail.com>
2023-05-19 14:09:53 +0000

    refactored files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:peer_to_peer_multimedia_sharing_application/ui/dashboard.dart';
+import 'package:peer_to_peer_multimedia_sharing_application/ui/dashboard.ui.dart';
 import 'ui/file_manager/homepage.file_manager.dart';
-import 'ui/login.dart';
-import 'ui/splash.dart';
+import 'ui/login.ui.dart';
+import 'ui/splash.ui.dart';
 
 void main() {
   runApp(MaterialApp(
